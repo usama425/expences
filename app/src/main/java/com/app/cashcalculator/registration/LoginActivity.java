51edc12fde8913161d52ece6d6a2a2e0555e046b
Usama <usama.abdullah425@gmail.com>
2021-04-17 22:55:16 +0500

    Initial commit

---
+++ app/src/main/java/com/app/cashcalculator/registration/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.app.cashcalculator.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.cashcalculator.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/app/cashcalculator/registration/SignupActivity.java
@@ -0,0 +1,18 @@
+package com.app.cashcalculator.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.cashcalculator.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+    }
+}
